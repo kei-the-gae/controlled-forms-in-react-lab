3528516a2b9ba19e0a890cc731be6bfef04db38b
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-15 13:21:08 -0700

    Add event handler to update books state, fix newBook's default state

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -5,12 +5,16 @@ const Bookshelf = () => {
         { title: 'Fourth Wing', author: 'Rebecca Yarros' },
         { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
     ]);
-    const [newBook, setNewBook] = useState([
-        { title: '', author: '' },
-    ]);
+    const [newBook, setNewBook] = useState({ title: '', author: '' });
 
     const handleInputChange = (e) => {
         setNewBook({ ...newBook, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setBooks([...books, newBook]);
+        setNewBook({ title: '', author: '' });
     };
 
     return (
